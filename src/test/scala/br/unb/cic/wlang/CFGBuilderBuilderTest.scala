99a640f6989be04763b9d250b643611c1fe72379
jclavo <jclavotafur@gmail.com>
2021-08-25 21:04:10 -0300

    add reverse flow and its tests

--- src/test/scala/br/unb/cic/wlang/CFGBuilderBuilderTest.scala
+++ src/test/scala/br/unb/cic/wlang/CFGBuilderBuilderTest.scala
@@ -57,4 +57,47 @@ class CFGBuilderBuilderTest extends AnyFunSuite {
     assert(expected == g)
   }
 
+  test("factorial-CFG-inverse") {
+    val d1 = Assignment("y", Var("x"), 1)
+    val d2 = Assignment("z", Const(1), 2)
+    val d3 = Assignment("z", Mult(Var("z"), Var("y")), 4)
+    val d4 = Assignment("y", Sub(Var("y"), Const(1)), 5)
+    val w1 = While(Condition(GT(Var("y"), Const(1)), 3), Sequence(d3, d4))
+    val d5 = Assignment("y", Const(0), 6)
+
+    val p = WhileProgram(Sequence(d1, Sequence(d2, Sequence(w1, d5))))
+
+    val g = CFGBuilder.flowInverse(p)
+
+    val expected: Set[(Int, Int)] =
+      Set((2, 1)
+         ,(3, 2)
+         ,(4, 3)
+         ,(5, 4)
+         ,(3, 5)
+         ,(6, 3))
+
+    assert(expected == g)
+  }
+
+  test("power-CFG-inverse") {
+    
+    val d1 = Assignment("z", Const(1), 1)
+    val d2 = Assignment("z", Mult(Var("z"), Var("y")), 3)
+    val d3 = Assignment("x", Sub(Var("x"), Const(1)), 4)
+    val w1 = While(Condition(GT(Var("x"), Const(0)), 2), Sequence(d2, d3))
+
+    val p = WhileProgram(Sequence(d1,w1))
+
+    val g = CFGBuilder.flowInverse(p)
+
+    val expected: Set[(Int, Int)] =
+      Set((2, 1)
+         ,(3, 2)
+         ,(4, 3)
+         ,(2, 4))
+
+    assert(expected == g)
+  }
+
 }
